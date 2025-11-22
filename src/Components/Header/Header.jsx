bc7f64b4584fdadc362bccce6e34b988393ce816
Navneet Rishav <98542381+xronae@users.noreply.github.com>
2025-11-22 18:32:36 +0530

    Change resume link in Header component
    Updated the resume link in the Header component.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -20,12 +20,13 @@ const Header = () => {
               </span><Cursor cursorStyle="_"/></p>
             <div className="buttons">
                 <button><a href="mailto:rishavnavneet@gmail.com">Hire Me</a></button>
-                <button><a href="https://drive.google.com/file/d/1InTVitk1az5fvaNFxRY4fZA_1lju7ckh/view?usp=drive_link" target='_blank'>Get Resume</a></button>
+                <button><a href="https://drive.google.com/file/d/1IbzSPIOcQ0mZsD6-GdflcvzAwYFvvA6E/view?usp=sharing" target='_blank'>Get Resume</a></button>
                 <button><a href="https://github.com/xronae" target='_blank'>Github Profile</a></button>
             </div>
         </div>
     </div>
   )
 }
+
 
 export default Header
