982ebab2c34a2e1d8a5fe3404aec45661402930d
Navneet Rishav <98542381+xronae@users.noreply.github.com>
2025-11-22 16:22:08 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,199 @@
+import header_img from './header_img.png'
+import MainDpLong from './MainDpLong.png'
+import logo from "./mainLogo.png"
+
+import python_ico from "./python_ico.png"
+import javascript_ico from "./javascript_ico.png"
+import reactjs_ico from "./reactjs_ico.png"
+import mysql_ico from "./mysql_ico.png"
+import bootstrap_ico from "./bootstrap_ico.png"
+import git_ico from "./git_ico.png"
+import github_ico from "./github_ico.png"
+import api_ico from "./api_ico.png"
+import tailwindCss_ico from "./tailwindCss_ico.png"
+import linkedin_ico from './linkedin-ico.png'
+import gmail_ico from './gmail-ico.png'
+
+import skillVid01 from './img_bg_vid.mp4'
+import skillVid02 from './img_bg_vid02.mp4'
+import skillVid03 from './img_bg_vid03.mp4'
+import skillVid04 from './img_bg_vid04.mp4'
+import skillVid05 from './img_bg_vid05.mp4'
+import skillVid06 from './img_bg_vid06.mp4'
+import skillVid07 from './img_bg_vid07.mp4'
+import skillVid08 from './img_bg_vid08.mp4'
+
+import projectSS_01 from './projectSS_01.png'
+import projectSS_02 from './projectSS_02.png'
+import projectSS_03 from './projectSS_03.png'
+
+import CertiImg01 from './Certi01.png'
+import CertiImg02 from './Certi02.png'
+import CertiImg03 from './Certi03.png'
+import CertiImg04 from './Certi04.png'
+import CertiImg05 from './Certi05.png'
+
+export const Certi01 = [
+    CertiImg01,
+    'Supervised Machine Learning: Regression and Classification',
+    'Oct, 2023',
+    'https://www.coursera.org/account/accomplishments/verify/TYHPTQQZLUKA?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse',
+    "Learned core ML concepts including regression, classification, and model evaluation",
+    "Implemented algorithms like linear/logistic regression using Python and NumPy.",
+    "Understood key ideas such as cost functions, gradients, overfitting, and regularization."
+]
+
+export const Certi02 = [
+    CertiImg02,
+    'Python Web Dev Pro: Flask, Django, HTML, CSS & Bootstrap ',
+    'oct, 2025',
+    'https://www.udemy.com/certificate/UC-5f4c8bcf-4672-4000-bc53-6e041576982b/',
+    " Completed Python Web Dev Pro course covering Flask, Django, HTML, CSS, and Bootstrap, strengthening both backend and frontend capabilities",
+    "Built dynamic web applications with Python-based servers, routing, templates, authentication, and database integration.",
+    "Gained hands-on experience creating responsive UIs and connecting them with robust backend logic — enabling end-to-end full-stack project development."
+]
+
+export const Certi03 = [
+    CertiImg03,
+    'CSS, Bootstrap, JavaScript And PHP Stack Complete Course',
+    'June, 2023',
+    'https://drive.google.com/file/d/1JDewzeNwHrb1yg0O5VgkJyIALXSCH7NM/view',
+    "Completed a comprehensive course covering CSS, Bootstrap, JavaScript, and PHP, strengthening both front-end and backend development skills.",
+    "Built responsive, interactive web interfaces using modern UI components and client-side scripting.",
+    "Developed dynamic PHP-based backends with form handling, basic CRUD operations, and server-side logic — enabling full-stack project creation from UI to database."
+]
+
+export const Certi04 = [
+    CertiImg04,
+    'Complete A.I. & Machine Learning, Data Science Bootcamp',
+    'Feb, 2024',
+    'https://www.udemy.com/certificate/UC-ee880017-fa0f-430c-8e0f-cfd03ef904e4/',
+    "Completed a comprehensive AI & Machine Learning and Data Science bootcamp, gaining strong fundamentals in data processing, model building, and analytical workflows",
+    "Learned to create ML models that can integrate into full-stack applications for features like recommendations, predictions, or automated insights.",
+    "Strengthened understanding of backend data pipelines, API integration with ML outputs, and building intelligent features into modern web applications."
+]
+
+export const Certi05 = [
+    CertiImg05,
+    'Artificial Intelligence Foundations: Machine Learning',
+    'March, 2023',
+    'https://www.linkedin.com/learning/certificates/476a9da56e9fc1a081c562737f2c585b0b53c47ecabdf0183d8afba38dfe4fe7?trk=share_certificate',
+    "Completed Artificial Intelligence Foundations: Machine Learning, gaining a strong understanding of core ML concepts and practical workflows.",
+    "Learned how supervised and unsupervised models operate, and how to prepare, analyze, and use data effectively.",
+    "Improved ability to integrate simple ML logic and insights into full-stack applications, enabling smarter backend features and data-driven web solutions."
+]
+
+export const assExp = {
+    header_img,
+    MainDpLong,
+    logo,
+    projectSS_01,
+    projectSS_02,
+    projectSS_03,
+    gmail_ico,
+    linkedin_ico,
+    github_ico,
+    CertiImg01,
+}
+
+export const skill_list = [
+    {
+        skill_name: "Python",
+        skill_image: python_ico,
+        skill_link: 'https://docs.python.org/3/'
+    },
+    {
+        skill_name: "JavaScript",
+        skill_image: javascript_ico,
+        skill_link: 'https://devdocs.io/javascript/'
+    },
+    {
+        skill_name: "ReactJS",
+        skill_image: reactjs_ico,
+        skill_link: 'https://legacy.reactjs.org/docs/getting-started.html'
+    },
+    {
+        skill_name: "TailWind CSS",
+        skill_image: tailwindCss_ico,
+        skill_link: 'https://v2.tailwindcss.com/docs'
+    },
+    {
+        skill_name: "My SQL",
+        skill_image: mysql_ico,
+        skill_link: 'https://dev.mysql.com/doc/'
+    },
+    {
+        skill_name: "Boot Strap",
+        skill_image: bootstrap_ico,
+        skill_link: 'https://getbootstrap.com/docs/4.1/getting-started/introduction/'
+    },
+    {
+        skill_name: "Git",
+        skill_image: git_ico,
+        skill_link: 'https://git-scm.com/doc'
+    },
+    {
+        skill_name: "Github",
+        skill_image: github_ico,
+        skill_link: 'https://docs.github.com/en'
+    },
+    {
+        skill_name: "Rest API",
+        skill_image: api_ico,
+        skill_link: 'https://docs.github.com/en/rest?apiVersion=2022-11-28'
+    }
+]
+
+export const skill_list_01 = [
+    {
+        skill_name: "Python",
+        skill_link: 'https://docs.python.org/3/',
+        skill_ext: ".py",
+        skill_vid: skillVid01
+    },
+    {
+        skill_name: "JavaScript",
+        skill_link: 'https://devdocs.io/javascript/',
+        skill_ext: ".js",
+        skill_vid: skillVid08
+    },
+    {
+        skill_name: "ReactJS",
+        skill_link: 'https://legacy.reactjs.org/docs/getting-started.html',
+        skill_ext: ".jsx",
+        skill_vid: skillVid03
+    },
+    {
+        skill_name: "TailWind CSS",
+        skill_link: 'https://v2.tailwindcss.com/docs',
+        skill_ext: ".css",
+        skill_vid: skillVid04
+    }
+]
+
+export const skill_list_02 = [
+    {
+        skill_name: "My SQL",
+        skill_link: 'https://dev.mysql.com/doc/',
+        skill_ext: ".sql",
+        skill_vid: skillVid02
+    },
+    {
+        skill_name: "TypeScript",
+        skill_link: 'https://www.typescriptlang.org/',
+        skill_ext: ".tsx",
+        skill_vid: skillVid06
+    },
+    {
+        skill_name: "Git and Github",
+        skill_link: 'https://git-scm.com/doc',
+        skill_ext: ".git",
+        skill_vid: skillVid05
+    },
+    {
+        skill_name: "Rest API",
+        skill_link: 'https://docs.github.com/en/rest?apiVersion=2022-11-28',
+        skill_ext: ".<>",
+        skill_vid: skillVid07
+    },
+]
